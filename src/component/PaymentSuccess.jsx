6b9a2000aaf05904df17da3411dc53e281f6f0f2
darshu07gupta <darshana.credencetech@gmail.com>
2024-03-09 11:35:42 +0530

    Created payment success and failure page

---
+++ src/component/PaymentSuccess.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function PaymentSuccess() {
+    return (
+        <div className="flex justify-center items-center h-screen bg-[#53940f80]">
+            <div className="text-center">
+                <h1 className="text-3xl font-bold mb-4">Payment Successful!</h1>
+                <p className="text-lg">Thank you for your purchase.</p>
+            </div>
+        </div>
+    );
+}
+
+export default PaymentSuccess;
