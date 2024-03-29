c2a9cac3a31096f0baa61173e605f34498e5ea10
Anuj Kumar <140053059+Anuj122000@users.noreply.github.com>
2024-03-29 19:03:13 +0530

    made changes in subscription and invoices pages accroding to the response chages

--- src/component/Invoices.jsx
+++ src/component/Invoices.jsx
@@ -11,9 +11,7 @@ const Invoices = () => {
   useEffect(() => {
     const fetchInvoices = async () => {
       try {
-        const url = `${
-          import.meta.env.VITE_SHOPIFY_API_URL
-        }/stripe/invoices/customers`;
+        const url = `${import.meta.env.VITE_SHOPIFY_API_URL}/stripe/invoices/customers`;
 
         const params = {
           email: userEmail,
@@ -37,61 +35,87 @@ const Invoices = () => {
 
     fetchInvoices();
   }, []);
+  function formatDate(isoDateString) {
+    const date = new Date(isoDateString);
+    const formattedDate = date.toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' });
+    const timeString = date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+    return `${formattedDate} ${timeString}`;
+  }
+  function formatAmountWithCurrency(currency, amount) {
+    let currencySymbol;
+    switch (currency) {
+      case 'usd':
+        currencySymbol = '$';
+        break;
+      case 'inr':
+        currencySymbol = '₹';
+        break;
+      // Add more cases for other currencies as needed
+      default:
+        currencySymbol = '';
+        break;
+    }
+    return `${currencySymbol} ${amount}`;
+  }
 
   return (
     <div>
       {invoices !== null ? (
-        Object.keys(invoices).length === 0 ? <DataNotFound/> : <section className="body-font relative">
-        <div className="container p-5 pt-10 mx-auto">
-          <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-100 ">
-                <tr>
-                  <th scope="col" className="px-6 py-3">
-                    Name
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Email
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Amount Paid
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Action
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {invoices.map((invoice, i) => (
-                  <tr
-                    className="odd:bg-white  even:bg-gray-50  border-b "
-                    key={i}
-                  >
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-                    >
-                      {invoice.customer_shipping.name}
+        Object.keys(invoices).length === 0 ? <DataNotFound /> : <section className="body-font relative">
+          <div className="container p-5 pt-10 mx-auto">
+            <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+              <table className="w-full text-sm text-left rtl:text-right text-gray-500 ">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-100 ">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">
+                      Name
                     </th>
-                    <td className="px-6 py-4">{invoice.customer_email}</td>
-                    <td className="px-6 py-4">{invoice.amount_paid}</td>
-                    <td className="px-6 py-4">
-                      <a
-                        href={invoice.invoice_pdf}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="px-2 py-1 font-semibold cursor-pointer leading-tight text-green-700 bg-green-100 rounded-sm"
-                      >
-                        Download
-                      </a>
-                    </td>
+                    <th scope="col" className="px-6 py-3">
+                      Email
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Created at
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Amount Paid
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                      Action
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {invoices.map((invoice, i) => (
+                    <tr
+                      className="odd:bg-white  even:bg-gray-50  border-b "
+                      key={i}
+                    >
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                      >
+                        {invoice?.name}
+                      </th>
+                      <td className="px-6 py-4">{invoice?.email}</td>
+                      <td className="px-6 py-4">{formatDate(invoice?.date)}</td>
+                      <td className="px-6 py-4">{formatAmountWithCurrency(invoice?.currency, invoice?.amount_paid)}</td>
+                      <td className="px-6 py-4">
+                        <a
+                          href={invoice?.invoice_pdf}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="px-2 py-1 font-semibold cursor-pointer leading-tight text-green-700 bg-green-100 rounded-sm"
+                        >
+                          Download
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
       ) : (
         <div
           className="flex justify-center items-center"
