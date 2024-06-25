7cfb6387e019493ff6853ec8a871c109c6137ef1
Prahlad Kumar Verma <prahlad.credencetech@gmail.com>
2024-06-25 11:52:39 +0530

    Fixed Loaded Position

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -25,7 +25,7 @@ import LoadingAnimation from "../component/Loader";
 import { totalQuantity } from "../utils";
 import Popup from "../component/Popup";
 import SpiceLevel from "../component/SpiceLevel";
-import cardIcon from '../assets/cartnew.png';
+import cardIcon from "../assets/cartnew.png";
 
 const Product = () => {
   const [apiResponse, setApiResponse] = useState(null);
@@ -417,8 +417,8 @@ const Product = () => {
           noMeal: "Subscription",
           price: "₹2110.12/meal",
           discountPrice: "2000.12/meal",
-        }
-      ]
+        },
+      ],
     },
     {
       id: 2,
@@ -440,8 +440,8 @@ const Product = () => {
           noMeal: "Subscription",
           price: "₹1210.12/meal",
           discountPrice: "₹1000.12/meal",
-        }
-      ]
+        },
+      ],
     },
     {
       id: 3,
@@ -463,17 +463,14 @@ const Product = () => {
           noMeal: "Subscription",
           price: "₹1210.12/meal",
           discountPrice: "₹1000.12/meal",
-        }
-      ]
+        },
+      ],
     },
-  ]
-
-
+  ];
 
   const handleChange = (event) => {
     setSelectedValue(event.target.value);
   };
-
 
   return (
     <>
@@ -481,8 +478,10 @@ const Product = () => {
         <div className="min-h-[75vh] w-full bg-[#f1663c]">
           <div className=" py-2 bg-[#FBAE36] w-full overflow-x-auto flex-1 whitespace-nowrap  scrollbar-hide flex lg:justify-between items-center">
             <div className="hidden lg:block ml-10">
-              <h3 className="text-[#231F20] font-skillet text-2xl lg:text-3xl">Meal Package</h3>
-              <div className="w-28" >
+              <h3 className="text-[#231F20] font-skillet text-2xl lg:text-3xl">
+                Meal Package
+              </h3>
+              <div className="w-28">
                 <select
                   id="selectDropdown"
                   value={selectedValue?.noMeal}
@@ -490,7 +489,11 @@ const Product = () => {
                   className="pl-3 pr-10 py-2 mt-1 text-[#231F20] font-skillet font-bold text-xl rounded-lg active:border-none bg-[#EADEC1]"
                 >
                   {options.map((option) => (
-                    <option className="text-[#231F20] font-skillet text-base" key={option.id} value={option}>
+                    <option
+                      className="text-[#231F20] font-skillet text-base"
+                      key={option.id}
+                      value={option}
+                    >
                       {option.noMeal}
                     </option>
                   ))}
@@ -499,7 +502,9 @@ const Product = () => {
             </div>
             <div className="flex flex-row items-center ">
               <div className="flex flex-row items-center gap-x-2 mr-10">
-                <p className="text-[#231F20] font-skillet text-2xl">Fill your box 📦</p>
+                <p className="text-[#231F20] font-skillet text-2xl">
+                  Fill your box 📦
+                </p>
               </div>
               <div className="flex flex-row gap-x-2 mr-2">
                 <div className="rounded border-dashed border-[#f1663c] border-[2px] flex justify-center items-center h-16 w-16">
@@ -524,7 +529,6 @@ const Product = () => {
               <div className="bg-[#f1663c] flex justify-center items-center rounded-tl-md rounded-bl-md h-16 w-12">
                 <img src={cardIcon} alt="" className="w-6 h-6" />
               </div>
-
 
               {/* <div className="flex h-12 lg:h-16 gap-5">
                 <div
@@ -621,9 +625,9 @@ const Product = () => {
             {/* <div className="mt-4 overflow-y-auto" style={{ height: '79vh' }} ref={productsContainerRef}> */}
             <div
               className="mt-4 container mx-auto"
-            // style={{
-            //   height: "62vh",
-            // }}
+              // style={{
+              //   height: "62vh",
+              // }}
             >
               {rawResonse.collections.edges.map((category, index) => (
                 // <div key={category.node.title} ref={productSectionsRefs[index]}>
@@ -712,88 +716,90 @@ const Product = () => {
                             </button>
                           </div>
                         ) : loading[product.node.variants.edges[0].node.id] ? (
-                          <svg
-                            width="80"
-                            height="80"
-                            viewBox="0 0 120 30"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="#4fa94d"
-                            data-testid="three-dots-svg"
-                          >
-                            <circle cx="15" cy="15" r="15">
-                              <animate
-                                attributeName="r"
-                                from="15"
-                                to="15"
-                                begin="0s"
-                                dur="0.8s"
-                                values="15;9;15"
-                                calcMode="linear"
-                                repeatCount="indefinite"
-                              ></animate>
-                              <animate
-                                attributeName="fill-opacity"
-                                from="1"
-                                to="1"
-                                begin="0s"
-                                dur="0.8s"
-                                values="1;.5;1"
-                                calcMode="linear"
-                                repeatCount="indefinite"
-                              ></animate>
-                            </circle>
-                            <circle
-                              cx="60"
-                              cy="15"
-                              r="9"
-                              attributeName="fill-opacity"
-                              from="1"
-                              to="0.3"
+                          <div className="flex items-center justify-center">
+                            <svg
+                              width="80"
+                              height="52"
+                              viewBox="0 0 120 30"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="#4fa94d"
+                              data-testid="three-dots-svg"
                             >
-                              <animate
-                                attributeName="r"
-                                from="9"
-                                to="9"
-                                begin="0s"
-                                dur="0.8s"
-                                values="9;15;9"
-                                calcMode="linear"
-                                repeatCount="indefinite"
-                              ></animate>
-                              <animate
-                                attributeName="fill-opacity"
-                                from="0.5"
-                                to="0.5"
-                                begin="0s"
-                                dur="0.8s"
-                                values=".5;1;.5"
-                                calcMode="linear"
-                                repeatCount="indefinite"
-                              ></animate>
-                            </circle>
-                            <circle cx="105" cy="15" r="15">
-                              <animate
-                                attributeName="r"
-                                from="15"
-                                to="15"
-                                begin="0s"
-                                dur="0.8s"
-                                values="15;9;15"
-                                calcMode="linear"
-                                repeatCount="indefinite"
-                              ></animate>
-                              <animate
+                              <circle cx="15" cy="15" r="15">
+                                <animate
+                                  attributeName="r"
+                                  from="15"
+                                  to="15"
+                                  begin="0s"
+                                  dur="0.8s"
+                                  values="15;9;15"
+                                  calcMode="linear"
+                                  repeatCount="indefinite"
+                                ></animate>
+                                <animate
+                                  attributeName="fill-opacity"
+                                  from="1"
+                                  to="1"
+                                  begin="0s"
+                                  dur="0.8s"
+                                  values="1;.5;1"
+                                  calcMode="linear"
+                                  repeatCount="indefinite"
+                                ></animate>
+                              </circle>
+                              <circle
+                                cx="60"
+                                cy="15"
+                                r="9"
                                 attributeName="fill-opacity"
                                 from="1"
-                                to="1"
-                                begin="0s"
-                                dur="0.8s"
-                                values="1;.5;1"
-                                calcMode="linear"
-                                repeatCount="indefinite"
-                              ></animate>
-                            </circle>
-                          </svg>
+                                to="0.3"
+                              >
+                                <animate
+                                  attributeName="r"
+                                  from="9"
+                                  to="9"
+                                  begin="0s"
+                                  dur="0.8s"
+                                  values="9;15;9"
+                                  calcMode="linear"
+                                  repeatCount="indefinite"
+                                ></animate>
+                                <animate
+                                  attributeName="fill-opacity"
+                                  from="0.5"
+                                  to="0.5"
+                                  begin="0s"
+                                  dur="0.8s"
+                                  values=".5;1;.5"
+                                  calcMode="linear"
+                                  repeatCount="indefinite"
+                                ></animate>
+                              </circle>
+                              <circle cx="105" cy="15" r="15">
+                                <animate
+                                  attributeName="r"
+                                  from="15"
+                                  to="15"
+                                  begin="0s"
+                                  dur="0.8s"
+                                  values="15;9;15"
+                                  calcMode="linear"
+                                  repeatCount="indefinite"
+                                ></animate>
+                                <animate
+                                  attributeName="fill-opacity"
+                                  from="1"
+                                  to="1"
+                                  begin="0s"
+                                  dur="0.8s"
+                                  values="1;.5;1"
+                                  calcMode="linear"
+                                  repeatCount="indefinite"
+                                ></animate>
+                              </circle>
+                            </svg>
+                          </div>
                         ) : (
                           <div
                             className="flex w-full justify-center gap-x-8 lg:gap-x-10 items-center rounded-lg mt-2 px-4 py-1 lg:py-2"
