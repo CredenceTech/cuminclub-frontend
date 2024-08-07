aa99eb86a3f0a6280115b7faa0718b62f3f87aea
Anuj Kumar <140053059+Anuj122000@users.noreply.github.com>
2024-08-07 15:49:54 +0530

    design recipe page and made responsive

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ladingImage from '../assets/landingbanner.png'
+import ladingImage from '../assets/biryani.png'
 import sahipanir from '../assets/sahipanir.png'
 const Recipes = () => {
 
@@ -43,13 +43,13 @@ const Recipes = () => {
                         </div>
                     </div>
                     <div className='col-span-1 lg:col-span-2'>
-                        <img src={ladingImage} alt="" />
+                        <img src={ladingImage} className='h-full w-full' alt="" />
                     </div>
                 </div>
             </div>
             <div className='bg-[#FAFAFA]'>
                 <div className='container bg-[#FAFAFA] mx-auto grid grid-cols-3 sticky top-0'>
-                    <div className='col-span-1 flex gap-x-11 py-3'>
+                    <div className='col-span-1 hidden md:flex gap-x-11 py-3'>
                         <div className=''>
                             <p className='text-[#757575]'>Prep Time</p>
                             <p className='text-[#231F20] text-lg'>10 mins</p>
@@ -59,7 +59,7 @@ const Recipes = () => {
                             <p className='text-[#231F20] text-lg'>25 mins</p>
                         </div>
                     </div>
-                    <div className='col-span-2 flex justify-between items-center'>
+                    <div className='col-span-3 md:col-span-2 flex overflow-x-auto flex-1 whitespace-nowrap  scrollbar-hide justify-between items-center'>
                         <div className='border-r-2 px-10 py-3 bg-[] border-r-[#C75801]'>
                             <p className='text-lg text-[#231F20]'> Step 1 <span className='text-[#757575] text-sm'> {"(2 mins)"}</span> </p>
                         </div>
@@ -75,7 +75,7 @@ const Recipes = () => {
                     </div>
                 </div>
                 <div className='container mx-auto grid grid-cols-3'>
-                    <div className='col-span-1'>
+                    <div className='pl-7 md:pl-3 col-span-3 md:col-span-1'>
                         <p className='text-4xl py-6 font-skillet'>Ingredients</p>
                         <ol className='pr-4 pb-7'>
                             {items.map((item, index) => (
@@ -86,7 +86,7 @@ const Recipes = () => {
                             ))}
                         </ol>
                     </div>
-                    <div className='pl-7 lg:pl-16 col-span-2'>
+                    <div className='pl-7 lg:pl-16 col-span-3 md:col-span-2'>
                         <p className='text-4xl p-6 font-skillet'>Instructions</p>
                         {InstructionsText?.map((item, i) => (
                             <div className='flex mb-5'>
@@ -94,7 +94,7 @@ const Recipes = () => {
                                     <p className='text-5xl text-[#e9bc9982] font-semibold'>{item?.id}</p>
                                 </div>
                                 <div className='border-b ml-4 border-b-[#C6C6C6]'>
-                                    <p className='text-xl pb-4 text-[#333333]  pr-40'>{item?.text}</p>
+                                    <p className='text-xl pb-4 text-[#333333] pr-5 md:pr-10  lg:pr-40'>{item?.text}</p>
                                 </div>
                             </div>
                         ))}
@@ -104,24 +104,28 @@ const Recipes = () => {
             </div>
             <div className='bg-[#FFFFFF] pt-7'>
                 <div className='container mx-auto '>
-                    <div className='flex justify-center items-center'>
-                        <div className='pr-20'>
-                            <img src={sahipanir} alt="" className='w-full  h-[400px]' />
+                    <div className='flex md:flex-row flex-col-reverse  justify-center items-center'>
+                        <div className='md:pr-20 flex justify-center items-center'>
+                            <img src={sahipanir} alt="" className='w-full h-[300px] md:h-[400px]' />
                         </div>
                         <div className='pl-8'>
                             <h1 className='text-[#C75801] font-skillet text-4xl '>Make this recipe Instantly Yours</h1>
                             <p>Made with our </p>
                             <h1 className='text-[#333333] font-skillet text-5xl '>Shahi biryani kit</h1>
-                            <p className='pb-32'>About the product</p>
-                            <div className='flex gap-5 pb-8' >
+                            <p className='pb-32 hidden md:flex'>About the product</p>
+                            <div className='hidden md:flex gap-5 pb-8' >
                                 <button className='px-4 rounded py-1 bg-[#231F20] text-sm text-[#FFFFFF]' type='button'>Add to Cart</button>
-                                <button className='px-4 rounded py-1 bg-[#231F20] text-sm text-[#FFFFFF]' type='button'>Buy Noew</button>
+                                <button className='px-4 rounded py-1 bg-[#231F20] text-sm text-[#FFFFFF]' type='button'>Buy Now</button>
                             </div>
-
                         </div>
                     </div>
-
-
+                    <div className='md:hidden flex-row'>
+                        <p className='pb-20 pl-8 pt-6'>About the product</p>
+                        <div className='gap-5 ' >
+                            <button className='px-4 w-1/2  py-4 bg-[#231F20] text-sm text-[#FFFFFF]' type='button'>Add to Cart</button>
+                            <button className='px-4 w-1/2  py-4 bg-[#C75801] text-sm text-[#FFFFFF]' type='button'>Buy Now</button>
+                        </div>
+                    </div>
                 </div>
             </div>
 
