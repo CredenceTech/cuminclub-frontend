5b4344ae77ce57263c48e2632f1d0e5d75c0f71d
Anuj Kumar <140053059+Anuj122000@users.noreply.github.com>
2024-09-28 18:45:31 +0530

    added video control in readt to eat page

--- src/pages/ReadyToEat.jsx
+++ src/pages/ReadyToEat.jsx
@@ -59,6 +59,7 @@ function ReadyToEat() {
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [showModel, setShowModel] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
+    const [playing, setPlaying] = useState(true);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -537,6 +538,14 @@ function ReadyToEat() {
         },
     ]
 
+    const handleVideoEnd = () => {
+        setPlaying(false);
+    };
+
+    const handlePlayAgain = () => {
+        setPlaying(true);
+    };
+
 
     return (
 
@@ -606,17 +615,30 @@ function ReadyToEat() {
                     </div>
 
                 </div>
-                <div className="col-span-1 h-[891px] w-full bg-[#0F0B05] ">
-                    {/* <img src={videoimage} alt="" className="h-full w-full" /> */}
+                <div className="col-span-1 relative h-[891px] w-full bg-[#0F0B05]">
+                    {/* {!playing && (
+                        <img
+                            src={videoimage}
+                            alt="Video Thumbnail"
+                            className="absolute inset-0 w-full h-full object-cover z-10"
+                        />
+                    )} */}
                     <ReactPlayer
-                        className='bg-cover'
+                        className="bg-cover"
                         url={kormavideo}
-                        width='100%'
-                        height='100%'
-                        controls={true}
-                        loop={true}
-                        playing={true}
+                        width="100%"
+                        height="100%"
+                        playing={playing}
+                        onEnded={handleVideoEnd}
                     />
+                    {!playing && (
+                        <div className="absolute inset-0 flex justify-center z-20 items-center">
+                            <button
+                                onClick={handlePlayAgain}
+                                className="text-white p-4 rounded-full">
+                                <svg xmlns="http://www.w3.org/2000/svg" height="48px" viewBox="0 -960 960 960" width="48px" fill="#EFEFEF"><path d="m383-310 267-170-267-170v340Zm97 230q-82 0-155-31.5t-127.5-86Q143-252 111.5-325T80-480q0-83 31.5-156t86-127Q252-817 325-848.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 82-31.5 155T763-197.5q-54 54.5-127 86T480-80Zm0-60q142 0 241-99.5T820-480q0-142-99-241t-241-99q-141 0-240.5 99T140-480q0 141 99.5 240.5T480-140Zm0-340Z" /></svg>                            </button>
+                        </div>
+                    )}
                 </div>
             </div>
 
