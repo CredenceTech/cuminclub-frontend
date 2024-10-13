e96ef04c7ac814fd93137846dcec2822c2bfeff4
Anuj Kumar <140053059+Anuj122000@users.noreply.github.com>
2024-10-13 12:09:50 +0530

    seach in header review and product details page animation on steps and padding and announcement part

---
+++ src/component/AnnouncementBar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+const AnnouncementBar = () => {
+    const announcements = [
+        'Use Code "DELICIOUS10" to get 10% off on your entire order',
+        'Free shipping on orders above $50!',
+        'Limited-time offer: Buy 1 Get 1 Free on select items!',
+    ];
+
+    const [currentAnnouncement, setCurrentAnnouncement] = useState(0);
+
+    // Handle next announcement
+    const nextAnnouncement = () => {
+        setCurrentAnnouncement((prevIndex) =>
+            prevIndex === announcements.length - 1 ? 0 : prevIndex + 1
+        );
+    };
+
+    // Handle previous announcement
+    const prevAnnouncement = () => {
+        setCurrentAnnouncement((prevIndex) =>
+            prevIndex === 0 ? announcements.length - 1 : prevIndex - 1
+        );
+    };
+
+    return (
+        <div className="flex items-center justify-between bg-[#EB7E01] text-white h-[53px] px-4">
+            {/* Previous Button */}
+            <button
+                onClick={prevAnnouncement}
+                className="text-xl font-bold focus:outline-none hover:text-gray-300"
+            >
+                <svg width="7" height="12" viewBox="0 0 7 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.8002 1.1333L1.2002 5.7333L5.80019 10.3333" stroke="white" stroke-width="1.53333" stroke-linecap="square" />
+                </svg>
+            </button>
+
+            {/* Announcement Text */}
+            <div className="text-center text-[#FFFFFF] font-[500] leading-[21.6px] text-[18px]">
+                {announcements[currentAnnouncement]}
+            </div>
+
+            {/* Next Button */}
+            <button
+                onClick={nextAnnouncement}
+                className="text-xl font-bold focus:outline-none hover:text-gray-300"
+            >
+                <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.9668 16.0999L14.5668 11.4999L9.9668 6.8999" stroke="white" stroke-width="1.53333" stroke-linecap="square" />
+                </svg>
+
+            </button>
+        </div>
+    );
+};
+
+export default AnnouncementBar;
